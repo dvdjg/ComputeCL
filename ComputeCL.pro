4b171a2c15ebb996babbd031043b85bbd7993a2d
dvdjg <dvdjg@hotmail.com>
2015-03-29 20:31:28 +0200

    first functional version

--- ComputeCL.pro
+++ ComputeCL.pro
@@ -1,35 +1,65 @@
+TARGET = ComputeCL
 TEMPLATE = app
 CONFIG += console
 CONFIG -= app_bundle
-CONFIG -= qt
+CONFIG -= qt rtti
+
+CONFIG(debug, debug|release):SUFFIX=d
+else:SUFFIX=
+
+TARGET=$$TARGET$$SUFFIX
+
+*-g++:QMAKE_TARGET.arch = $$QMAKE_HOST.arch
+*-g++-32:QMAKE_TARGET.arch = x86
+*-g++-64:QMAKE_TARGET.arch = x86_64
+
+SPEC=$$basename(QMAKESPEC)
+SPEC=$$replace(SPEC, win32, windows)-$$QMAKE_TARGET.arch
+
+LIBDIR=$$PWD/../lib
+staticlib {
+    DEFINES += STATIC
+    DESTDIR=$$LIBDIR
+} else {
+    # Binaries and dynamic libs goes to bin
+    DESTDIR=$$PWD/../bin
+    static:DESTDIR=$$DESTDIR-static
+}
+
+LIBDIR=$$LIBDIR/$$SPEC
+DESTDIR=$$DESTDIR/$$SPEC
+
+LIBS += "-L$$LIBDIR"
+
 WD=$$PWD
 win32:{
     INCLUDEPATH += "C:/Program Files (x86)/AMD APP SDK/2.9-1/include" "C:\Program Files (x86)\AMD APP SDK\3.0-0-Beta\include"
     INCLUDEPATH += C:/Programa/boost_1_57_0 "$$WD/../boost_1_57_0"
-    INCLUDEPATH += "$$WD/../compute/include"
     LIBS += "-LC:/Programa/boost_1_57_0/stage/lib" "-L$$WD/../boost_1_57_0/stage/lib"
 
-    win32-*:contains(QMAKE_HOST.arch, x86_64):{
+    message("arch=$$QMAKE_TARGET.arch")
+    win32-*:contains(QMAKE_HOST.arch, x86_64) {
         LIBS += "-LC:/Program Files (x86)/AMD APP SDK/2.9-1/lib/x86_64" "-LC:/Program Files (x86)/AMD APP SDK/3.0-0-Beta/lib/x86_64"
-        message("64 bits build")
     } else {
         LIBS += "-LC:/Program Files (x86)/AMD APP SDK/2.9-1/lib/x86" "-LC:/Program Files (x86)/AMD APP SDK/3.0-0-Beta/lib/x86"
-        message("32 bits build")
     }
 } else {
     INCLUDEPATH += /home/puesto/Programa/compute/include /opt/AMDAPPSDK-3.0-0-Beta/include
     LIBS += "-L/opt/AMDAPPSDK-3.0-0-Beta/lib/x86"
 
-    linux-*:contains(QMAKE_HOST.arch, x86_64):{
-        message("64 bits build")
+    linux-*:contains(QMAKE_TARGET.arch, x86_64) {
     } else {
-        message("32 bits build")
     }
 }
 
-LIBS += -lOpenCL
+DEFINES += _SCL_SECURE_NO_WARNINGS
+INCLUDEPATH += "$$WD/../compute/include" "$$WD/../half"
+
+LIBS += -lOpenCL -lhalf$$SUFFIX
 
 SOURCES += main.cpp
 
 include(deployment.pri)
 qtcAddDeployment()
+
+
--- main.cpp
+++ main.cpp
@@ -9,10 +9,161 @@
 //---------------------------------------------------------------------------//
 
 #include <iostream>
+#include <algorithm>
+
+#define CL_USE_DEPRECATED_OPENCL_2_0_APIS
+#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+#define CL_USE_DEPRECATED_OPENCL_1_1_APIS
+
+#include <math.h>
+#include <CL/cl.h>
 
 #include <boost/compute/core.hpp>
+#include <boost/compute/system.hpp>
+#include <boost/compute/image/image2d.hpp>
+#include <boost/compute/utility/dim.hpp>
+#include <boost/compute/utility/source.hpp>
+
+#include <boost/compute/command_queue.hpp>
+#include <boost/compute/exception/opencl_error.hpp>
+#include <boost/compute/image/image_format.hpp>
+#include <boost/exception/diagnostic_information.hpp>
+
+#include "half.h"
 
 namespace compute = boost::compute;
+
+inline void box_filter_image(const compute::image2d &input,
+                             compute::image2d &output,
+                             compute::uint_ box_height,
+                             compute::uint_ box_width,
+                             compute::command_queue &queue)
+{
+    using compute::dim;
+
+    const compute::context &context = queue.get_context();
+
+    // simple box filter kernel source
+    const char source[] = BOOST_COMPUTE_STRINGIZE_SOURCE(
+        __kernel void box_filter(__read_only image2d_t input,
+                                 __write_only image2d_t output,
+                                 uint box_height,
+                                 uint box_width)
+        {
+            int x = get_global_id(0);
+            int y = get_global_id(1);
+//                    // djg
+                    const sampler_t sampler = CLK_ADDRESS_NONE | CLK_FILTER_NEAREST;
+                    float4 value = read_imagef(input, sampler, (int2)(x, y));
+                    value += (float4)( 1.1f, 1.1f, 1.1f, 1.1f );
+                    write_imagef(output, (int2)(x, y), value);
+
+//                    int h = get_image_height(input);
+//                    int w = get_image_width(input);
+//                    int k = box_width;
+//                    int l = box_height;
+
+//            if(x < k/2 || y < l/2 || x >= w-(k/2) || y >= h-(l/2)){
+//                write_imagef(output, (int2)(x, y), (float4)(0, 0, 0, 1));
+//            }
+//            else {
+//                const sampler_t sampler = CLK_ADDRESS_NONE | CLK_FILTER_NEAREST;
+
+//                float4 sum = { 0, 0, 0, 0 };
+//                for(int i = 0; i < k; i++){
+//                    for(int j = 0; j < l; j++){
+//                        sum += read_imagef(input, sampler, (int2)(x+i-k, y+j-l));
+//                    }
+//                }
+//                sum /= (float) k * l;
+//                float4 value = (float4)( sum.x, sum.y, sum.z, 1.f );
+//                write_imagef(output, (int2)(x, y), value);
+//            }
+        }
+    );
+
+    // build box filter program
+    compute::program program = compute::program::create_with_source(source, context);
+    program.build();
+
+    // setup box filter kernel
+    compute::kernel kernel(program, "box_filter");
+    kernel.set_arg(0, input);
+    kernel.set_arg(1, output);
+    kernel.set_arg(2, box_height);
+    kernel.set_arg(3, box_width);
+
+    // execute the box filter kernel
+    queue.enqueue_nd_range_kernel(kernel, dim(0, 0), input.size(), dim(1, 1));
+}
+
+// this example shows how to load an image using Qt, apply a simple
+// box blur filter, and then display it in a Qt window.
+void halfCL()
+{
+    size_t width = 1024;
+    size_t height = 1024;
+
+    std::cout << "height:" << height << std::endl
+              << "width:" << width << std::endl;
+
+    // create compute context
+    compute::device gpu = compute::system::default_device();
+    compute::context context(gpu);
+    compute::command_queue queue(context, gpu);
+    std::cout << "\nHALF\n====\ndevice: " << gpu.name() << std::endl;
+
+    // get the opencl image format for the qimage
+    compute::image_format format = compute::image_format(compute::image_format::r, compute::image_format::float16);
+
+    // create input and output images on the gpu
+    compute::image2d input_image(context, width, height, format);
+    compute::image2d output_image(context, width, height, format);
+
+    const size_t origin[3] = { 0, 0, 0 };
+    const size_t region[3] = { width, height, 1 };
+    size_t row_pitch = 0;
+
+    void *pRawIn = queue.enqueue_map_image(input_image,
+                                           compute::command_queue::map_write,
+                                           origin,
+                                           region,
+                                           &row_pitch);
+
+    half *pImageIn = static_cast<half*>(pRawIn);
+    for(size_t h = 0; h < height; ++h) {
+        for(size_t w = 0; w < width; ++w) {
+            half & hLuminance = pImageIn[h*width + w];
+            float fLuminance = sinf(sqrtf((h*h+w*w)/100.f));
+            hLuminance = fLuminance;
+        }
+    }
+    queue.enqueue_unmap_buffer(input_image, pRawIn);
+
+    // apply box filter
+    box_filter_image(input_image, output_image, 7, 7, queue);
+
+    // copy gpu blurred image from to host qimage
+    void *pRawOut = queue.enqueue_map_image(output_image,
+                                            compute::command_queue::map_read,
+                                            origin,
+                                            region,
+                                            &row_pitch);
+    half *pImageOut = static_cast<half*>(pRawOut);
+    float fError = 0.f;
+    for(size_t h = 0; h < height; ++h) {
+        for(size_t w = 0; w < width; ++w) {
+            half & hLuminance = pImageOut[h*width + w];
+            float fStored = hLuminance;
+            float fLuminance = sinf(sqrtf((h*h+w*w)/100.f));
+            float fDiff = fStored - fLuminance;
+            fError += fDiff;
+        }
+    }
+    queue.enqueue_unmap_buffer(output_image, pRawOut);
+    std::cout << "Error = " << fError << std::endl;
+}
+
 
 int main()
 {
@@ -42,6 +193,7 @@ int main()
                 << "  vendor: " << device.vendor() << std::endl
                 << "  profile: " << device.profile() << std::endl
                 << "  version: " << device.version() << std::endl
+                << "  version_number: " << device.version_number() << std::endl
                 << "  driver_version: " << device.driver_version() << std::endl
                 << "  address_bits: " << device.address_bits() << std::endl
                 << "  global_memory_size: " << device.global_memory_size()/(1024*1024) << " MiB" << std::endl
@@ -57,5 +209,12 @@ int main()
         }
     }
 
+    try {
+        halfCL();
+    } catch(boost::exception const&  bex) {
+        std::cerr << "Boost Exception: " << boost::diagnostic_information(bex);
+    } catch(std::exception const&  ex) {
+        std::cerr << "Std Exception: " << ex.what();
+    }
     return 0;
 }
